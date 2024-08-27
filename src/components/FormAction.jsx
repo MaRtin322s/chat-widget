2937d2af8caaedf0c193c5380d923b6b2f039227
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-08-27 13:17:52 +0300

    useTransition hook

--- src/components/FormAction.jsx
+++ src/components/FormAction.jsx
@@ -1,23 +1,22 @@
+import { useTransition } from 'react';
+import { useState } from 'react';
 import { useFormStatus } from 'react-dom';
 
 const updateName = (name) => new Promise((resolve) => setTimeout(() => resolve(name), 2000));
 
-function SubmitButton() {
-    const { pending } = useFormStatus();
-
-    return <button disabled={pending}>Submit</button>
-}
-
 function FormAction() {
-    const submitAction = async (formData) => {
-        await updateName(formData.get('name'));
-        console.log(formData.get('name'));
+    const [pending, startTransition] = useTransition();
+    const submitAction = (formData) => {
+        startTransition(async () => {
+            const name = await updateName(formData.get('name'));
+            console.log(name);
+        });
     }
 
     return (
         <form action={submitAction}>
             <input type="text" name="name" autoComplete={'true'} />
-            <SubmitButton />
+            <button disabled={pending ? true : false}>Submit</button>
         </form>
     );
 }
