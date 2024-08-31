d43b8d33bf223a5a11b9364216045bff10a4538b
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-08-31 20:29:37 +0300

    form action

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import FormAction from "./components/FormAction";
-import Optimistic from "./components/Optimistic";
 
 function App() {
     return (
         <>
             <div>
-                {/* <FormAction /> */}
-                <Optimistic />
+                <FormAction />
             </div>
         </>
     )
--- src/components/FormAction.jsx
+++ src/components/FormAction.jsx
@@ -1,31 +1,18 @@
-import { useTransition, useActionState } from 'react';
-
-const updateName = (name) => new Promise((resolve) => setTimeout(() => resolve(name), 2000));
-
 function FormAction() {
-    const [pending, startTransition] = useTransition();
-    const submitAction = (formData) => {
-        startTransition(async () => {
-            const name = await updateName(formData.get('name'));
-            console.log(name);
-        });
+    const formAction = (formData) => {
+        const name = formData.get('name');
+        console.log(name);
+
     }
-    
-    const formHandler = async (previousState, formData) => {
-        const name = await updateName(formData.get('name'));
-        previousState.name = name;
-        
-        return previousState;
-    }
-    
-    const [state, formAction, isPending] = useActionState(formHandler, { name: ''});
-    console.log(state);
-    
+
     return (
-        <form>
-            <input type="text" name="name" autoComplete={'true'} />
-            <button formAction={formAction} disabled={isPending ? true : false}>Submit</button>
-        </form>
+        <>
+            <h1>Form Action</h1>
+            <form action={formAction}>
+                <input type="text" name="name" />
+                <button>Submit</button>
+            </form>
+        </>
     );
 }
 
